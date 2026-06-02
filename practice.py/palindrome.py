eb89942710815cebc53eb313f12bd1e6ff6c771e
chaalika112 <101463360+chaalika112@users.noreply.github.com>
2026-06-02 16:27:02 -0500

    Delete practice.py directory

--- practice.py/palindrome.py
+++
@@ -1,7 +0,0 @@
-word = input("Enter anything to check: ")
-
-if word == word[::-1]:
-    print("It is a palindrome")
-
-else:
-    print("It is not a palindrome")
--- practice.py/pow_function.py
+++
@@ -1,4 +0,0 @@
-n = int(input("Enter a number:"))
-P = int(input("Enter the power:"))
-
-print(n ** P)
