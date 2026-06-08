f037ebe264439fbc72608fa3030fd1e230659ac6
Chaalika <chaalikachukka112@gmail.com>
2026-06-07 20:14:48 -0500

    random variables program

--- practice.py/random_variable.py
+++ practice.py/random_variable.py
@@ -1,8 +1,11 @@
 import random
 
-print (random.randrange(1,11))
+print (random.randint(1,11))
 
 names = ["Pavan", "Raji", "Chaalika", "Manasa"]
 
 print(random.choice(names))
 
+dice = random.randint(1, 6)
+
+print("Dice rolled:", dice)
