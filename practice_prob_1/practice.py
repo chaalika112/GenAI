15642569960a85ca53868a7f7f5d9358173b820d
Chaalika <chaalikachukka112@gmail.com>
2026-06-14 22:28:48 -0500

    Changes

---
+++ practice_prob_1/practice.py
@@ -0,0 +1,15 @@
+#Dictionary
+
+employee = {
+
+"id": 101,
+
+"name": "Alice",
+
+"salary": 50000
+
+}
+
+employee["Department"] = "IT"
+employee["salary"] = 55000
+print(employee)
---
+++ python_practice/without_len.py
@@ -0,0 +1,9 @@
+students = {
+    "student1": {"name": "Chaalika", "age": 22},
+    "student2": {"name": "Chaalika", "age": 22}
+}
+
+count = 0 
+for student in students:
+    count = count + 1
+    print(count)
